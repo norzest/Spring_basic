d623bbdbcd99d0dcda6392ef43a115e9703451e6
영효 심 <dark1203@naver.com>
2021-07-30 17:58:35 +0900

    2021-07-30

---
+++ src/main/java/hello/hellospring/controller/SampleController.java
@@ -0,0 +1,19 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.mapper.SampleMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+    @Autowired
+    SampleMapper sampleMapper;
+
+    @GetMapping("sample")
+    public String sampleView() {
+        System.out.println(sampleMapper.selectSampleData());
+        return "sample";
+    }
+
+}
